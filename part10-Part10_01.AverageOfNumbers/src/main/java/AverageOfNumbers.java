17bc152f2a9b9ceca194a61dea021d9d08956816
nikolas-bott <nikolasbott@gmail.com>
2024-07-19 16:57:43 +0200

    Completed the first two tasks of Part10 - learned the basics of streams

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,7 +6,22 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         // Write your program here
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while(true){
+            String row = scanner.nextLine();
+            if(row.equals("end")){
+                break;
+            }
+            
+            list.add(row);
+        }
+        double average = list.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .average()
+                .getAsDouble();
+        System.out.println("average of the numbers: "+average);
 
     }
 }
--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -5,8 +5,35 @@ import java.util.Scanner;
 public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
-
+        
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while(true){
+            String string = scanner.nextLine();
+            if(string.equals("end")){
+                break;
+            }
+            list.add(string);
+        }
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String input = scanner.nextLine();
+        if(input.equals("n")){
+            double averageOfNegative = list.stream()
+                    .mapToDouble(s -> Double.valueOf(s))
+                    .filter(number -> number < 0)
+                    .average()
+                    .getAsDouble();
+            System.out.println("Average of negative numbers: "+averageOfNegative);
+        }
+        
+        if(input.equals("p")){
+            double averageOfPositiv = list.stream()
+                    .mapToDouble(s -> Double.valueOf(s))
+                    .filter(number -> number > 0)
+                    .average()
+                    .getAsDouble();
+            System.out.println("Average of positive numbers: "+averageOfPositiv);
+        }
     }
 }
